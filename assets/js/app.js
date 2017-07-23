cf3fed30e9c631dfdfb7f3a321f6cf7542511bd4
rpanczer <bobby.panczer@arcadis-us.com>
2017-07-22 22:09:52 -0400

    Add form formating items

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ define([
        , "collections/snippets" , "collections/my-form-snippets"
        , "views/tab" , "views/my-form"
        , "text!data/input.json", "text!data/radio.json", "text!data/select.json", "text!data/buttons.json"
-       , "text!templates/app/render.html",  "text!templates/app/about.html", 
+       , "text!templates/app/render.html",  "text!templates/app/about.html",
 ], function(
   $, _, Backbone
   , SnippetsCollection, MyFormSnippetsCollection
@@ -22,22 +22,6 @@ define([
       new TabView({
         title: "Radios / Checkboxes"
         , collection: new SnippetsCollection(JSON.parse(radioJSON))
-      });
-      new TabView({
-        title: "Select"
-        , collection: new SnippetsCollection(JSON.parse(selectJSON))
-      });
-      new TabView({
-        title: "Buttons"
-        , collection: new SnippetsCollection(JSON.parse(buttonsJSON))
-      });
-      new TabView({
-        title: "Rendered"
-        , content: renderTab
-      });
-      new TabView({
-        title: "About"
-        , content: aboutTab
       });
 
       //Make the first tab active!
